23086aa7514ca2a4fe8c26b8c89836e4e40f25b5
13BenCar <ben.lee.carty@gmail.com>
2022-12-19 15:03:46 +0000

    Added Service layer files

---
+++ NorthwindCustomerAPI/NorthwindCustomerAPI/Models/Services/CustomerService.cs
@@ -0,0 +1,44 @@
+﻿namespace NorthwindCustomerAPI.Models.Services
+{
+    public class CustomerService
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerService()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public CustomerService(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateCustomer(Customer c)
+        {
+            _context.Add(c);
+            _context.SaveChanges();
+        }
+
+        public Customer GetCustomerById(string customerId)
+        {
+            return _context.Customers.Find(customerId);
+        }
+
+        public List<Customer> GetCustomerList()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public void RemoveCustomer(Customer c)
+        {
+            _context.Remove(c);
+            _context.SaveChanges();
+        }
+
+        public void SaveCustomerChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ NorthwindCustomerAPI/NorthwindCustomerAPI/Models/Services/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthwindCustomerAPI.Models.Services
+{
+    public interface ICustomerService
+    {
+        List<Customer> GetCustomerList();
+        Customer GetCustomerById(string customerId);
+        void CreateCustomer(Customer c);
+        void SaveCustomerChanges();
+        void RemoveCustomer(Customer c);
+    }
+}
