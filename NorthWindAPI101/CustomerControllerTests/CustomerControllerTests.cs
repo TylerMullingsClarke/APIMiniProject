0ad5f3c4cc32fa50e7680e8bc255edd73a2ccae2
13BenCar <ben.lee.carty@gmail.com>
2022-12-20 15:37:09 +0000

    Added Service and ability to create multiple customers

---
+++ NorthWindAPI101/CustomerControllerTests/CustomerControllerTests.cs
@@ -0,0 +1,25 @@
+using NorthWindAPI101;
+using NorthWindAPI101.Controllers;
+using NorthWindAPI101.Models;
+using NorthWindAPI101.Models.DTO;
+
+namespace NorthwindAPITests
+{
+    public class CustomerControllerTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        [Category("Create Tests")]
+        public void GivenNewCustomer_PostCustomer_AddsNewCustomerToTable()
+        {
+            
+        
+        }
+    }
+}
---
+++ NorthWindAPI101/NorthWindAPI101/Models/Services/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using NorthWindAPI101.Models.DTO;
+
+namespace NorthWindAPI101.Models.Services
+{
+    public interface ICustomerService
+    {
+        List<CustomerDTO> GetCustomerDTOList();
+        Customer GetCustomerById(string customerId);
+        CustomerDTO GetCustomerDTOById(string customerId);
+        void CreateCustomer(Customer c);
+        void SaveCustomerChanges();
+        void RemoveCustomer(Customer c);
+    }
+}
