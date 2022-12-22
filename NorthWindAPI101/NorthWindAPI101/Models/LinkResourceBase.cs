f75ec016cbb856f6283463fd2cafd6fa3658f3b4
clark234 <clark234@cucollege.coventry.ac.uk>
2022-12-22 10:05:20 +0000

    Start of HATEOUS

---
+++ NorthWindAPI101/NorthWindAPI101/Models/LinkResourceBase.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthWindAPI101.Models
+{
+    public class LinkResourceBase
+    {
+        public LinkResourceBase()
+        {
+        }
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+}
